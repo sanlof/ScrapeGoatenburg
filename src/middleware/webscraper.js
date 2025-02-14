ccb6a22fa9eb7679ec546f41356d93bc69c3efc2
Andreawingardh <andreawingardh@gmail.com>
2025-02-14 16:10:21 +0100

    Updated puppeteer code
    Reworked code to include pressing the cookie button, identifying the show all events button and identifying the back to start button before page.content() is saved and sent into cheerio for scrape

--- src/middleware/webscraper.js
+++ src/middleware/webscraper.js
@@ -10,9 +10,7 @@ let eventResults = []
 
 
 
-
-
-const htmlContent = await getContent();
+getContent();
 
 export default eventResults;
 
@@ -20,58 +18,96 @@ async function getContent() {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.goteborg.com/evenemang');
+    await page.goto('https://www.goteborg.com/evenemang', { waitUntil: 'networkidle2'});
 
     await page.setViewport({width: 1080, height: 1024});
-    await page.waitForSelector('.btn.btn--theme-link.btn--pill');
-    console.log('waited');
-    // await page.locator('.btn.btn--theme-link.btn--pill').fill("Visa alla evenemang");
-    await page.click('.btn.btn--theme-link.btn--pill');
-    console.log('button clicked');
+    // await page.waitForSelector('.btn.btn--theme-link.btn--pill');
+    // console.log('waited');
+    // // await page.locator('.btn.btn--theme-link.btn--pill').fill("Visa alla evenemang");
+    // await page.click('.btn.btn--theme-link.btn--pill');
+    // console.log('button clicked');
     
 // Use page.evaluate to find the button with specific text and click it
-//     await page.waitForSelector('button');
-//     const button = await page.evaluate(() => {
-//     const buttons = Array.from(document.querySelectorAll('button')); // Get all buttons
-//     const targetButton = buttons.find(button => button.textContent.trim() === 'Visa alla evenemang'); // Replace 'Click Me' with the button text you're targeting
-//   });
+    // await page.waitForSelector('button');
 
-//   if (button) {
-//     // Click the button if found
-//     page.locator(button).click();
-//     console.log('Button clicked!');
+    const cookieButton = await page.evaluate(() => {
+        const buttons = Array.from(document.querySelectorAll('button')); // Get all buttons
+       const cookieButton = buttons.find(button => button.textContent.trim() === 'Acceptera alla');
+        if(cookieButton)
+            {
+                cookieButton.click();
+                console.log('clickec cookie button');
+                }  else {
+                console.log('cookie button not found')
+            }
+    });
+
+    console.log('cookie-button');
+    const backToStartButton = await page.evaluate(() => {
+        const buttons = Array.from(document.querySelectorAll('btn--theme-link')); // Get all buttons
+        const backToStartButton = buttons.find(button => button.textContent.trim() === 'Tillbaka till start'); 
+        if(backToStartButton)
+            {
+                console.log('back to start arrived');
+                }  else {
+                console.log('back to start not found');
+            }
+    });
+
+    console.log('back-to-start-button identified')
     
 
+    const showAllButton = await page.evaluate(() => {
+    const buttons = Array.from(document.querySelectorAll('button')); // Get all buttons
+    const showAllButton = buttons.find(button => button.textContent.trim() === 'Visa alla evenemang'); 
+    if (showAllButton) {
+        showAllButton.click();
+        console.log('Button clicked!');
+      } else {
+        console.log('button not clicked');
+      }
+     
+  });
+
+    console.log('showallbutton pressed')
+
+
+    page.screenshot({ path: 'screenshot.png' });
+    console.log('image saved');
+    const htmlContent = await page.content();
+
+
+
+  console.log('page loaded')
+
+  const $ = cheerio.load(htmlContent);
+
+            
+  $('.event-card__body').each((index, element) => {
+  const title = $(element).find('.heading').text().trim()
+  let link = $(element).find('a.stretched-link').attr('href');
+  link = 'https://www.goteborg.com' + link;
+  const date = $(element).find('.c-icon__title').eq(0).text();
+  const location = $(element).find('.c-icon__title').eq(1).text();
   
-  const htmlContent = page.content();
-  await browser.close();
-  return htmlContent;
+  const eventData = { 
+          'title': title,
+          'date': date,
+          'location': location,
+          'link': link
+      }
 
-}
+  eventResults.push(eventData);
+  
+  })
+ 
+
+await browser.close();
+
+  }
 
 
-    const $ = cheerio.load(htmlContent);
-            
-            $('.image-card-alt__body').each((index, element) => {
-            const title = $(element).find('.heading').text().trim()
-            let link = $(element).find('a.stretched-link').attr('href');
-            link = 'https://www.goteborg.com' + link;
-            const date = $(element).find('.c-icon__title').eq(0).text();
-            const location = $(element).find('.c-icon__title').eq(1).text();
-            
-            const eventData = { 
-                    'title': title,
-                    'date': date,
-                    'location': location,
-                    'link': link
-                }
-    
-            eventResults.push(eventData);
-            
-            })
-           
-
-    
+   
     
 
 
