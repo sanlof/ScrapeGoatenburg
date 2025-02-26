132bf344c780a0b9d096fd8c0d2c826c9f0abca5
Andreawingardh <andreawingardh@gmail.com>
2025-02-26 16:17:16 +0100

    Added a rendering of the pug template page event.pug

--- src/routes/event.js
+++ src/routes/event.js
@@ -1,19 +1,14 @@
 import express from 'express';
 import singlePageScrape from '../controller/singlepagescrape.js';
-import path from "path"
-import url from "url";
-
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const router = express.Router();
 
 router.get('/:slug', async (req, res) => {
     const { slug } = req.params;
     try {
-      const eventData = await singlePageScrape(slug);
-        // res.json({ data: eventData });
-        // console.log(eventData.text);
-        // res.send(eventData.text);
+        const eventData = await singlePageScrape(slug);
+        console.log(eventData.link)
+        res.render('event.pug', {eventData})
 
     } catch (error) {
       res.status(500).json({ error: error.message });
